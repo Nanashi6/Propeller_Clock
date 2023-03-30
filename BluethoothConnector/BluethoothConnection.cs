79b61be1401232e68f011ce69123b7b9669e42da
Юрий Ёвженко <devmail1071@gmail.com>
2023-03-30 23:32:34 +0300

    BluethoothUpdate

---
+++ BluethoothConnector/BluethoothConnection.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Sockets;
+using System.IO.Ports;
+
+namespace ArduinoBluethooth
+{
+    public class BluetoothConnection
+    {
+        private readonly string _portName;
+        private SerialPort _serialPort;
+
+        public BluetoothConnection(string portName)
+        {
+            _portName = portName;
+        }
+        /*
+        public static List<string> Search()
+        {
+            var ports = SerialPort.GetPortNames();
+            var result = new List<string>();
+
+            foreach (var port in ports)
+            {
+                try
+                {
+                    var serialPort = new SerialPort(port);
+                    serialPort.Open();
+                    var name = serialPort.ReadExisting();
+                    serialPort.Close();
+                    if (!string.IsNullOrEmpty(name))
+                    {
+                        result.Add(name.Trim());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error reading from port {port}: {ex.Message}");
+                }
+            }
+
+            return result;
+        }
+        */
+        public bool Connect()
+        {
+            var ports = SerialPort.GetPortNames();
+
+            if (!ports.Contains(_portName))
+            {
+                Console.WriteLine($"Error: Port {_portName} not found");
+                return false;
+            }
+
+            _serialPort = new SerialPort(_portName, 9600, Parity.None, 8, StopBits.One);
+            _serialPort.Open();
+
+            Console.WriteLine($"Connected to {_portName}");
+
+            return true;
+        }
+
+        public void Disconnect()
+        {
+            if (_serialPort == null)
+            {
+                Console.WriteLine($"Error: Port {_portName} not connected");
+                return;
+            }
+
+            _serialPort.Close();
+
+            Console.WriteLine($"Disconnected from {_portName}");
+        }
+
+        public string Read()
+        {
+            if (_serialPort == null)
+            {
+                Console.WriteLine($"Error: Port {_portName} not connected");
+                return null;
+            }
+
+            var message = _serialPort.ReadLine();
+
+            Console.WriteLine($"Received message: {message}");
+
+            return message;
+        }
+
+        public void Write(string message)
+        {
+            if (_serialPort == null)
+            {
+                Console.WriteLine($"Error: Port {_portName} not connected");
+                return;
+            }
+
+            _serialPort.WriteLine(message);
+
+            Console.WriteLine($"Sent message: {message}");
+        }
+    }
+}
--- Clock and text/Clock/Clock.ino
+++ Clock and text/Clock/Clock.ino
@@ -1,14 +1,14 @@
-const int pins[] = {3,4,5,6,7,8,9,10}; // Пины светодиодных сегментов
+const int pins[] = {3,4,5,6,7,8,9,10,11,12,A0,A1,A2,A3}; // Пины светодиодных сегментов
 int hallPin = 2;                     // Пин для считывания значения датчика Холла
-int hallVal = HIGH;                   // Исходное значение считывания датчика Холла
+int hallVal = HIGH;//LOW можно поставить                   // Исходное значение считывания датчика Холла
 
 int i, j;
 
-int minSecArrow[] = {0,0,1,1,1,1,1,1};
+int minSecArrow[] = {0,0,0,0,1,1,1,1,1,1,1,1,1,1};
 int secIndex = 2;
 int minIndex = 45;
 
-int hourArrow[] = {0,0,0,0,1,1,1,1};
+int hourArrow[] = {0,0,0,0,0,0,0,0,1,1,1,1,1,1};
 int hourIndex = 0;
 
 //volatile int revolutions = 0; // Количество оборотов
@@ -17,68 +17,68 @@ unsigned long currentTime = 0;  // Текущее время
 unsigned long elapsedTime = 0;  // Время, прошедшее с последнего изменения положения стрелок
 unsigned long pause = 0;        // Пауза между изменениями положения стрелок
 
-int ledMatrix[60][8] = 
+int ledMatrix[60][14] = 
   {
-    {HIGH,HIGH,HIGH,LOW,LOW,LOW,LOW,LOW}, //3
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,HIGH,LOW,LOW,LOW,LOW,LOW,LOW}, //4
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,HIGH,LOW,LOW,LOW,LOW,LOW,LOW},  //5
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,HIGH,HIGH,LOW,LOW,LOW,LOW,LOW}, //6
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,HIGH,LOW,LOW,LOW,LOW,LOW,LOW}, //7
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,HIGH,LOW,LOW,LOW,LOW,LOW,LOW},  //8
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,HIGH,HIGH,LOW,LOW,LOW,LOW,LOW}, //9
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,HIGH,LOW,LOW,LOW,LOW,LOW,LOW}, //10
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,HIGH,LOW,LOW,LOW,LOW,LOW,LOW},  //11
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,HIGH,HIGH,LOW,LOW,LOW,LOW,LOW}, //12
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,HIGH,LOW,LOW,LOW,LOW,LOW,LOW}, //1
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,HIGH,LOW,LOW,LOW,LOW,LOW,LOW},  //2
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
-    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,HIGH,HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW}, //0
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW}, //1
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},  //2
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,HIGH,HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW}, //3
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW}, //4
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},  //5
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,HIGH,HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW}, //6
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW}, //7
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},  //8
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,HIGH,HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW}, //9
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW}, //10
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},  //11
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW},
   };
 
 void setup() 
@@ -90,7 +90,7 @@ void setup()
 
   pinMode(hallPin, INPUT);
 
-//FALLING - регистрация изменения сигнала от HIGH к LOW    
+  //FALLING - регистрация изменения сигнала от HIGH к LOW    
   attachInterrupt(digitalPinToInterrupt(hallPin), countRevolutions, FALLING); // Настраиваем прерывание для подсчета оборотов
 
   Serial.begin(9600);
@@ -98,7 +98,7 @@ void setup()
 
 void loop() 
 {
-  drawClock();
+  //drawClock();
   /*
   if (elapsedTime >= 1000) { //Подсчет rmp раз в секунду, МБ понадобится
     float rpm = ((float)revolutions / (float)elapsedTime) * 60000.0;
@@ -108,18 +108,7 @@ void loop()
     lastTime = currentTime;
   }
   */
-}
-
-void countRevolutions() {
-  //revolutions++;  
-  currentTime = millis();
-  elapsedTime += currentTime - lastTime;
-  //pause = (currentTime - lastTime) / 120;
-  lastTime = currentTime;
-}
-
-// Отрисовка циферблата
-void drawClock() {
+  
   for (i = 0; i < 60; i++) {
     if(i == secIndex || i == minIndex){
       for (j = 7; j > 2; j--) { // перебираем элементы массива                
@@ -131,7 +120,7 @@ void drawClock() {
         digitalWrite(pins[j], hourArrow[j]); // зажигаем или гасим светодиод в соответствии с элементом массива
       }           
     }       
-    for (j = 0; j < 3; j++) { // перебираем элементы массива                                                   
+    for (j = 0; j < 3; j++) { // перебираем элементы массива (первые три лампы, т.к. они только они меняются на циферблате)                                                  
       digitalWrite(pins[j], ledMatrix[i][j]); // зажигаем или гасим светодиод в соответствии с элементом массива
     }
     
@@ -151,12 +140,63 @@ void drawClock() {
       }
     }
     
-    //delayMicroseconds(pause);
+    delayMicroseconds(pause);
     
-    for (j = 0; j < 8; j++) { // перебираем элементы массива                                                      //(первые три лампы, т.к. они только они меняются на циферблате)
+    for (j = 0; j < 14; j++) { // перебираем элементы массива                                                      
       digitalWrite(pins[j], LOW); // зажигаем или гасим светодиод в соответствии с элементом массива
     }
     
-    //delayMicroseconds(pause);
+    delayMicroseconds(pause);
+  }
+}
+
+void countRevolutions() {
+  //revolutions++;  
+  currentTime = millis();
+  elapsedTime += currentTime - lastTime;
+  pause = (currentTime - lastTime) / 120;
+  lastTime = currentTime;
+}
+
+// Отрисовка циферблата
+void drawClock() {
+  for (i = 0; i < 60; i++) {
+    if(i == secIndex || i == minIndex){
+      for (j = 7; j > 2; j--) { // перебираем элементы массива                
+        digitalWrite(pins[j], minSecArrow[j]); // зажигаем или гасим светодиод в соответствии с элементом массива
+      }     
+    }
+    else if(i == hourIndex){
+      for (j = 7; j > 3; j--) { // перебираем элементы массива                                                    
+        digitalWrite(pins[j], hourArrow[j]); // зажигаем или гасим светодиод в соответствии с элементом массива
+      }           
+    }       
+    for (j = 0; j < 3; j++) { // перебираем элементы массива (первые три лампы, т.к. они только они меняются на циферблате)                                                  
+      digitalWrite(pins[j], ledMatrix[i][j]); // зажигаем или гасим светодиод в соответствии с элементом массива
+    }
+    
+    if (elapsedTime > 1000){
+      elapsedTime = 0;
+      secIndex++;
+      if(secIndex == 60){
+        secIndex = 0;
+        minIndex++;
+        if(minIndex == 60){
+          minIndex = 0;
+          hourIndex+=5;
+          if(hourIndex == 60){
+            hourIndex = 0;                                        
+          }                
+        }
+      }
+    }
+    
+    delayMicroseconds(pause);
+    
+    for (j = 0; j < 14; j++) { // перебираем элементы массива                                                      
+      digitalWrite(pins[j], LOW); // зажигаем или гасим светодиод в соответствии с элементом массива
+    }
+    
+    delayMicroseconds(pause);
   }
 }
