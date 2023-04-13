28e7c88587ec125ad9b99d2989256b68e06e1cdb
Nanashi6 <yuevzhenko@gmail.com>
2023-04-13 23:16:18 +0300

    TextToMatrix Update

--- Sender/BluetoothContext.cs
+++ Sender/BluetoothContext.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InTheHand.Net.Sockets;
 
 namespace Sender
 {
     public class BluetoothContext
     {
+        public BluetoothContext() { }
+         
+        
     }
 }
